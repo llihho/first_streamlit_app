dda6489f2b259d8677083730f25ba7db3c67f097
llihho <liuhao80@gmail.com>
2022-06-21 22:23:39 +1000

    Update streamlit_app.py
    add all the lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilded Free-Range Egg')
